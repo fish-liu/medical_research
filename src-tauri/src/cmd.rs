a694b89281ec34cdc5fc2798bf778b8ea753041f
fish_liu <752023460@qq.com>
2025-01-16 15:48:27 +0800

    换行输入框

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -1,8 +1,11 @@
-use serde::{Deserialize, Serialize};
-use tauri::Manager;
-use crate::{qianfan::{ChatBuilder, ChatMessage}, QianFanHandler};
 use crate::Storage;
+use crate::{
+    qianfan::{ChatBuilder, ChatMessage},
+    QianFanHandler,
+};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use tauri::Manager;
 
 #[tauri::command]
 pub fn greet(name: &str) -> String {
@@ -10,8 +13,7 @@ pub fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-pub fn get_chat_type<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> String{
-
+pub fn get_chat_type<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> String {
     let storage = app.state::<Storage<R>>();
     let chat_type = storage.store.get("chat_type".to_string());
 
@@ -19,74 +21,77 @@ pub fn get_chat_type<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> String{
         Some(val) => {
             return val.to_string();
         }
-        None =>{
+        None => {
             return "".to_string();
         }
     }
 }
 
 #[tauri::command]
-pub fn set_chat_type<R: tauri::Runtime>(app: tauri::AppHandle<R>,chat_type: &str) ->String{
-    println!("----chat_type-----{}",chat_type);
+pub fn set_chat_type<R: tauri::Runtime>(app: tauri::AppHandle<R>, chat_type: &str) -> String {
+    println!("----chat_type-----{}", chat_type);
     let storage = app.state::<Storage<R>>();
     storage.store.set("chat_type".to_string(), json!(chat_type));
 
     "SUCCESS".to_string()
 }
 
-#[derive(Deserialize,Serialize)]
-pub struct ChatResult{
-    code:i32,
+#[derive(Deserialize, Serialize)]
+pub struct ChatResult {
+    code: i32,
 
-    message:Option<String>,
+    message: Option<String>,
 
-    data:Option<String>
+    data: Option<String>,
 }
 
-
 #[tauri::command]
-pub async fn chat<R: tauri::Runtime>(app: tauri::AppHandle<R>, messages:Vec<ChatMessage>) -> ChatResult{
-    println!("--messages len --- : {}",messages.len());
+pub async fn chat<R: tauri::Runtime>(
+    app: tauri::AppHandle<R>,
+    messages: Vec<ChatMessage>,
+) -> ChatResult {
+    println!("--messages len --- : {}", messages.len());
 
     if messages.len() > 0 {
-        
         let qianfan_handler = &app.state::<QianFanHandler>();
         let chat_builder: ChatBuilder = qianfan_handler.qianfan_client.clone().chat_completion();
 
         let storage = app.state::<Storage<R>>();
-        let chat_type = storage.store.get("chat_type".to_string()).unwrap().to_string();
+        let chat_type = storage
+            .store
+            .get("chat_type".to_string())
+            .unwrap()
+            .to_string();
 
         //let response = chat_builder.add_message("user".to_string(), "北京在那里".to_string()).execute().await;
-        let response = chat_builder.add_message_list(messages).chat_type(chat_type).execute().await;
-        println!("-------{:?}",response);
+        let response = chat_builder
+            .add_message_list(messages)
+            .chat_type(chat_type)
+            .execute()
+            .await;
+        println!("-------{:?}", response);
 
         match response {
             Ok(res) => {
-                return ChatResult{
-                    code:200,
-                    message:None,
-                    data:Some(res.result)
+                return ChatResult {
+                    code: 200,
+                    message: None,
+                    data: Some(res.result),
                 };
             }
-            Err(e) =>{
-                return ChatResult{
-                    code:0,
-                    message:Some(e.to_string()),
-                    data:None
+            Err(e) => {
+                return ChatResult {
+                    code: 0,
+                    message: Some(e.to_string()),
+                    data: None,
                 };
             }
         }
-    }else {
-        return ChatResult{
-            code:100,
-            message:Some("参数为空".to_string()),
-            data:None,
+    } else {
+        return ChatResult {
+            code: 100,
+            message: Some("参数为空".to_string()),
+            data: None,
         };
     }
-
-
 }
-
-
-
-
